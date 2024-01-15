a009727bdf4e4670172fa0718e05c89d2c4a237f
Veliko Kardziev <veliko@sharemagazines.de>
2024-01-15 15:36:58 +0100

    Add @vercel/speed-insights and update dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Roboto } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -10,6 +12,8 @@ const roboto = Roboto({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={roboto.className}>
+      <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </main>
   );
