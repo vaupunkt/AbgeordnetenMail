961cd8d909f45d7499e8af27862b64fd078f1666
Veliko Kardziev <veliko@sharemagazines.de>
2025-01-27 15:34:51 +0100

    Refactor Footer component to use consistent quotation marks and clean up unused code

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -57,72 +57,58 @@ export default function Footer() {
         justifyContent: "space-between",
       }}>
       <Box>
-        <Typography variant='h6' align='justify'>
+        <Typography variant="h6" align="justify">
           Impressum (Angaben gemäß § 5 TMG)
         </Typography>
         <Typography
           sx={{ userSelect: "none" }}
-          variant='body1'
-          align='justify'
-          aria-label='Veliko Kardziev'>
+          variant="body1"
+          align="justify"
+          aria-label="Veliko Kardziev">
           Veliko Kardziev
         </Typography>
         <Typography
           sx={{ userSelect: "none" }}
-          variant='body1'
-          align='justify'
-          aria-label='Neureutherstr. 4'>
-          Neureutherstr. 4
-        </Typography>
-        <Typography
-          sx={{ userSelect: "none" }}
-          variant='body1'
-          align='justify'
-          aria-label='80799 München'>
-          80799 München
-        </Typography>
-        <Typography
-          sx={{ userSelect: "none" }}
-          variant='body1'
-          align='justify'
-          aria-label='E-Mail: mail@velikokardziev.de'>
+          variant="body1"
+          align="justify"
+          aria-label="E-Mail: mail@velikokardziev.de">
           E-Mail: mail@velikokardziev.de
         </Typography>
         {userAgent === "mobile" ? (
           <Box sx={{ margin: "15px 0" }}>
             <Button
-              variant='outlined'
-              color='primary'
+              variant="outlined"
+              color="primary"
               onClick={handleShare}
               startIcon={<Share />}
-              aria-label='Diese Seite weiterleiten'>
+              aria-label="Diese Seite weiterleiten">
               Diese Seite weiterleiten
             </Button>
           </Box>
         ) : null}
       </Box>
       <Box sx={{ maxWidth: "500px" }}>
-        <Typography variant='body2' align='justify'>
+        <Typography variant="body2" align="justify">
           Diese Webseite steht nicht in Verbindung mit der Petition{" "}
-          <a href='https://innn.it/afdverbot' aria-label='Prüft ein AFD Verbot'>
+          <a href="https://innn.it/afdverbot" aria-label="Prüft ein AFD Verbot">
             "Prüft ein AFD Verbot" auf https://innn.it/afdverbot
           </a>{" "}
           sondern ist ein reines Zusatzangebot, erstellt mit öffentlich
           zugänglichen Daten:
         </Typography>
-        <Typography variant='caption' aria-label='Datenlizenz Deutschland'>
+        <Typography variant="caption" aria-label="Datenlizenz Deutschland">
           (c) Die Bundeswahlleiterin, Wiesbaden 2024 Datenlizenz Deutschland –
           Namensnennung – Version 2.0 (
           <a
-            href='https://www.govdata.de/dl-de/by-2-0'
-            aria-label='Datenlizenz Deutschland Link'>
+            href="https://www.govdata.de/dl-de/by-2-0"
+            aria-label="Datenlizenz Deutschland Link">
             https://www.govdata.de/dl-de/by-2-0
           </a>
           )
         </Typography>
         <Typography
-          variant='caption'
-          aria-label='Ergebnisse der Wahlbezirksstatistik'>
+          variant="caption"
+          aria-label="Ergebnisse der Wahlbezirksstatistik">
           Ergebnisse der Wahlbezirksstatistik zur Bundestagswahl 2021 Endgültig
           gewählte Bewerberinnen und Bewerber bei der Wahl zum 20. Deutschen
           Bundestag (26. September 2021) sowie Mitgliedschaftsverluste und
